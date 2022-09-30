68f9bb39266946b760c93791925cd5cfda72e124
RaulHerrera22 <shaulihot@gmail.com>
2022-09-30 10:41:45 -0300

    arreglo

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,44 +1,38 @@
 import { Injectable } from '@angular/core';
-import{HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from "rxjs"
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-task';
 
 const httpOpcions = {
-headers:  new HttpHeaders({
-  'Content-Type':'application/json'
-})
-}
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:4200/tasks'
-  
-  
-  constructor(
-    private http:HttpClient
-    ) { }
-  
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl)
+    return this.http.get<Task[]>(this.apiUrl);
   }
 
-  deleteTask(task: Task): Observable<Task>{
+  deleteTask(task: Task): Observable<Task> {
     const url = '${this.apiUrl}/${task.id}';
-    return this.http.delete<Task>(url)
+    return this.http.delete<Task>(url);
   }
 
   updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
-    return this.http.put<Task>(url, task, httpOpcions)
+    return this.http.put<Task>(url, task, httpOpcions);
   }
 
-  addTask(task:Task): Observable<Task>{
+  addTask(task: Task): Observable<Task> {
     return this.http.post<Task>(this.apiUrl, task, httpOpcions);
   }
 }
