1d884a9e90aacd7901971524315b489a960a543b
RaulHerrera22 <shaulihot@gmail.com>
2022-09-30 10:17:56 -0300

    ver

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -14,7 +14,7 @@ headers:  new HttpHeaders({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks'
+  private apiUrl = 'http://localhost:4200/tasks'
   
   
   constructor(
